9dc6c9836d004b49e60414a380a9ef7670f86b76
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2018-03-03 12:04:43 +0000

    Updated travis.yml

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 ApiKey=$1
 Source=$2
 
-dotnet nuget pack ./test-repo.nuspec --no-build /p:PackageVersion=1.0.0 -v d
+dotnet pack ./test-repo.nuspec --no-build /p:PackageVersion=1.0.0 -v d
 dotnet nuget push ./test-repo.*.nupkg -k $ApiKey -s $Source
