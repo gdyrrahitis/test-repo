540be8eb08cc87af8a5f7a9e40907653bcf59c7f
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2018-03-03 15:20:02 +0000

    Updated travis.yml

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,7 @@ ApiKey=$1
 Source=$2
 Version=$3
 
-dotnet pack ./test-repo.csproj -c Release --no-build --no-restore -o . /p:NuspecFile=test-repo.nuspec /p:PackageVersion=$Version -v d
-dotnet nuget push ./gdyrra.test.repo-todelete.$Version.nupkg -k $ApiKey -s $Source
+# dotnet pack ./test-repo.csproj -c Release --no-build --no-restore -o . /p:NuspecFile=test-repo.nuspec /p:PackageVersion=$Version -v d
+# dotnet nuget push ./gdyrra.test.repo-todelete.$Version.nupkg -k $ApiKey -s $Source
+mono nuget pack -Prop Configuration=Release -Version $Version
+mono nuget push ./gdyrra.test.repo-todelete.$Version.nupkg -ApiKey $ApiKey -Source $Source
