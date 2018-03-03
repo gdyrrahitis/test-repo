9ee51bc85d8eef0b8ac5c78056b231357bd52e5a
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2018-03-03 12:59:09 +0000

    Updated travis.yml

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ ApiKey=$1
 Source=$2
 
 dotnet pack ./test-repo.csproj -c Release --no-build --no-restore -o . /p:NuspecFile=test-repo.nuspec /p:PackageVersion=1.0.0 -v d
-dotnet nuget push ./test-repo-todelete.1.0.0.nupkg -k $ApiKey -s $Source
+dotnet nuget push ./gdyrra.test-repo-todelete.1.0.0.nupkg -k $ApiKey -s $Source
